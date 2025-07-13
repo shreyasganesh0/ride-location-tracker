cfdf440cc559510d2cdcbeaf5af28dc68a7e022e
ArchWayland <vansi.ganeshs@gmail.com>
2025-07-13 18:17:39 -0400

    feat: implement websocket echo server

--- internal/handler/ws.go
+++ internal/handler/ws.go
@@ -18,13 +18,32 @@ var upgrader = websocket.Upgrader {
 
 func WsHandler(w http.ResponseWriter, r *http.Request) {
 
-	_, err := upgrader.Upgrade(w, r, nil);
+	conn, err := upgrader.Upgrade(w, r, nil);
 	if err != nil {
 
-		log.Println("Error while performing ws handshake:", err);
+		log.Printf("Error while performing ws handshake: %v\n", err);
 		return;
 	}
-
+	defer conn.Close()
 	log.Println("Established websocket connetion");
+
+	for {
+
+		msg_typ, message, err := conn.ReadMessage()
+		if err != nil {
+
+			log.Printf("Error reading message in ws, closing conneciton: %v\n", err);
+			break;
+		}
+
+		err_write := conn.WriteMessage(msg_typ, message);
+		if err_write != nil {
+
+			log.Printf("Failed writing message on websocket: %v\n", err_write);
+			break
+		}
+
+	}
+
 	return;
 }
